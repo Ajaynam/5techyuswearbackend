a8c8b5f8cbf1b394f745c6577dabfb64ad67bd8a
Ajaynam <ajaynamdev701@gmail.com>
2024-05-22 15:45:20 +0530

    order add again

--- models/db.js
+++ models/db.js
@@ -5,15 +5,15 @@ const pool = mysql.createPool({
 
 
 
-  // host: '103.191.209.34',
-  // user: "ubzrnkmd_nyus_admin",
-  // password: "Win@nyus",
-  // database: "ubzrnkmd_ecom_nyus",
+  host: '103.191.209.34',
+  user: "ubzrnkmd_nyus_admin",
+  password: "Win@nyus",
+  database: "ubzrnkmd_ecom_nyus",
 
-  host: 'localhost' ,
-  user: "root",
-  password: "",
-  database: "eyeflow",
+  // host: 'localhost' ,
+  // user: "root",
+  // password: "",
+  // database: "eyeflow",
 
   
 });
